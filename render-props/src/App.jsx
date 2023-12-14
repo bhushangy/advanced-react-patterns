3fee91a733a78fc2cc39deac2b52538c86454c17
Bhushan G Y <bhushan.benz@gmail.com>
2023-12-14 13:11:09 +0530

    feat: added more example for render props

--- render-props/src/App.jsx
+++ render-props/src/App.jsx
@@ -101,3 +101,165 @@ export default function App() {
     </div>
   );
 }
+
+
+/*
+
+function Input(props) {
+
+  const [value, setValue] = useState("");
+
+
+  return (
+
+    <>
+
+      <input
+
+        type="text"
+
+        value={value}
+
+        onChange={e => setValue(e.target.value)}
+
+        placeholder="Temp in °C"
+
+      />
+
+      {props.render(value)}
+
+    </>
+
+  );
+
+}
+
+
+export default function App() {
+
+  return (
+
+    <div className="App">
+
+      <h1>☃️ Temperature Converter 🌞</h1>
+
+      <Input
+
+        render={value => (
+
+          <>
+
+            <Kelvin value={value} />
+
+            <Fahrenheit value={value} />
+
+          </>
+
+        )}
+
+      />
+
+    </div>
+
+  );
+
+}
+
+
+function Kelvin({ value }) {
+
+  return <div className="temp">{parseInt(value || 0) + 273.15}K</div>;
+
+}
+
+
+function Fahrenheit({ value }) {
+
+  return <div className="temp">{(parseInt(value || 0) * 9) / 5 + 32}°F</div>;
+
+}
+
+
+*/
+
+
+/*
+With children prop
+
+function Input(props) {
+
+  const [value, setValue] = useState(0);
+
+
+  return (
+
+    <>
+
+      <input
+
+        type="number"
+
+        value={value}
+
+        onChange={e => setValue(e.target.value)}
+
+        placeholder="Temp in °C"
+
+      />
+
+      {props.children(value)}
+
+    </>
+
+  );
+
+}
+
+
+export default function App() {
+
+  return (
+
+    <div className="App">
+
+      <h1>☃️ Temperature Converter 🌞</h1>
+
+      <Input>
+
+        {value => (
+
+          <>
+
+            <Kelvin value={value} />
+
+            <Fahrenheit value={value} />
+
+          </>
+
+        )}
+
+      </Input>
+
+    </div>
+
+  );
+
+}
+
+
+function Kelvin({ value }) {
+
+  return <div className="temp">{parseInt(value || 0) + 273.15}K</div>;
+
+}
+
+
+function Fahrenheit({ value }) {
+
+  return <div className="temp">{(parseInt(value || 0) * 9) / 5 + 32}°F</div>;
+
+}
+
+
+
+*/
